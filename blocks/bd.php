391ffbada4ca83dbac637dda06c084c435389043
KillToTheReal <73242190+KillToTheReal@users.noreply.github.com>
2020-11-11 17:01:46 +0400

    Update bd.php

--- blocks/bd.php
+++ blocks/bd.php
@@ -1,1 +1,9 @@
+<?php
 
+try {
+$db = new pdo('mysql:host=127.0.0.1; dbname=lab2_bd', 'pma' , '' );
+    }catch(PDOException $err) {
+        die('Connection Error :' . $err->getMessage() );
+    }
+
+?>
