37761dc3a03b1ead4f10502df46ad36c58523e5c
KillToTheReal <73242190+KillToTheReal@users.noreply.github.com>
2020-11-11 18:04:12 +0400

    Update bd.php

--- blocks/bd.php
+++ blocks/bd.php
@@ -1,8 +1,16 @@
 <?php
 
+$mysql = new mysqli('127.0.0.1' , 'pma' , '' , 'lab2_bd');
+
+if ($mysql->connect_errno) 
+    {
+    printf("Соединение не удалось: %s\n", $mysql->connect_error);
+    exit();
+    }
+
 try {
-$db = new pdo('mysql:host=127.0.0.1; dbname=lab2_bd', 'pma' , '' );
-    }catch(PDOException $err) {
+        $db = new pdo('mysql:host=127.0.0.1; dbname=lab2_bd', 'pma' , '' );
+    }   catch(PDOException $err) {
         die('Connection Error :' . $err->getMessage() );
     }
 
