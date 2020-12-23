9db5e8e186893f17818de36066ee647e76041efd
KillToTheReal <73242190+KillToTheReal@users.noreply.github.com>
2020-12-23 21:57:45 +0400

    Update check-test.php

--- enterForm/check-test.php
+++ enterForm/check-test.php
@@ -5,11 +5,21 @@
 
     require_once "../blocks/safety.php";  
 
+    echo('js код для проверки. если в php коде убрать преобразование с помощью ф-ии, на экран выведется слово WORKED c помощью алерта <br>');
+    
+    $test = "<script> alert('WORKED'); </script> ";
+    
+    $test_1 = safety_request_html($test);
+    
+    echo($test_1);
+    
+    echo('<br> <br> <p></p>');
+
     echo 'Данные до экранирования: ';
 
     print_r($_POST);
 
-    safety_request($_POST);
+    safety_request_bd($_POST);
 
     echo '<br>Данные после экранирования: ';
 
