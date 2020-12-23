a84420b8f08689a3e4112161d78f07d5e213c61c
KillToTheReal <73242190+KillToTheReal@users.noreply.github.com>
2020-12-23 21:12:17 +0400

    Update safety.php

--- blocks/safety.php
+++ blocks/safety.php
@@ -1,21 +1,12 @@
 <?php
     function safety_request(array &$object) //Применение ко всему выбранному массиву
     {
-        $subject = array ("/\"/", "/\'/" , "/\;/", "/\:/","/x00/", "/x1a/"); //Словарь шаблонов
-        
-        $replace = array ("\\\"", "\\\'" , "\\\;", "\\\:", "\\x00", "\\x1a"); //Словарь замен
-
+       
         foreach($object as &$member) 
         {
-            
-        $member = preg_replace('/\s+/', ' ', $member);  //Удаление пробелов
-
         $member = trim($member);
         
-        $member = preg_quote($member); //Экранирование служебных символов 
-        
-        $member = preg_replace($subject, $replace , $member); //Экранирование служебных символов не входящих в quote
-        
+            $member = mysqli_real_escape_string($member);
     }
     unset($member);
     }
