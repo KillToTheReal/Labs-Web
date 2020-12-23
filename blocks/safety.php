2e0318ca9c513bdb7a11363660dacb63a9777a83
KillToTheReal <73242190+KillToTheReal@users.noreply.github.com>
2020-12-23 21:56:28 +0400

    Update safety.php

--- blocks/safety.php
+++ blocks/safety.php
@@ -1,13 +1,29 @@
 <?php
-    function safety_request(array &$object) //Применение ко всему выбранному массиву
+
+    require_once "../blocks/bd.php";
+
+    function safety_request_bd(array &$object) 
     {
-       
+        $connect = mysqli_connect("127.0.0.1", "pma", "" , "lab2_bd");
+
         foreach($object as &$member) 
         {
+
+        $member = preg_replace('/\s+/', ' ', $member);
+
         $member = trim($member);
         
-            $member = mysqli_real_escape_string($member);
+        $member = mysqli_real_escape_string($connect, $member);
+        
     }
     unset($member);
     }
+
+
+
+    function safety_request_html($object)
+    {
+        return htmlspecialchars($object,ENT_QUOTES);
+    }
+    
 ?>
